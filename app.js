2a3f514cdef2e9e11b3d3000addb3304b0f6489c
Husien adel <husien@gmail.com>
2021-08-07 23:00:55 +0300

    v 1.0

---
+++ app.js
@@ -0,0 +1,77 @@
+let mybackground;
+let color1 = document.querySelector('#color1');
+let color2 = document.querySelector('#color2');
+let direction = document.querySelector('#direction');
+let code = document.querySelector('#code');
+let form = document.querySelector('#form');
+let codeWrap = document.querySelector('.codeWrap');
+let copyCode=document.querySelector('.copy');
+
+
+form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+
+
+
+    let color1Val = color1.value;
+    let color2Val = color2.value;
+    let directionVal = direction.value;
+
+  
+    mybackground = `linear-gradient(to ${directionVal}, ${color1Val}, ${color2Val})`;
+    
+    
+    codeWrap.style.display='block';
+    // code.textContent = mybackground;
+
+
+    document.body.style.backgroundImage = mybackground;
+
+    
+ 
+    copyText();
+
+
+
+})
+
+
+function copyText() {
+
+
+
+    copyCode.addEventListener('click', (e) => {
+        e.preventDefault();
+        let text = mybackground;
+        let textInput;
+
+        if (document.querySelector('textarea')===null) {
+         textInput = document.createElement('textarea');
+     
+        }
+
+        if (textInput.value.length==0) {
+            console.log(textInput.value.length);
+            textInput.value = text;
+            codeWrap.append(textInput);
+
+            textInput.select();
+            document.execCommand("copy");
+            console.log(textInput.value.length);
+
+            alert('code copied successfully');
+
+
+        } else if (textInput.value.length > 0) {
+            console.log(textInput.value.length);
+            textInput.value = text;
+
+            textInput.select();
+            document.execCommand("copy");
+            alert('code copied successfully');
+
+        }
+
+        
+    })
+}
