c8b0b290c70637092a20f7490b9e37d8faaa1a90
Husien adel <husien@gmail.com>
2021-08-09 19:15:22 +0300

    update js minor

--- app.js
+++ app.js
@@ -1,79 +1,54 @@
 let mybackground;
-let color1 = document.querySelector('#color1');
-let color2 = document.querySelector('#color2');
-let direction = document.querySelector('#direction');
-let code = document.querySelector('#code');
-let form = document.querySelector('#form');
-let codeWrap = document.querySelector('.codeWrap');
-let copyCode=document.querySelector('.copy');
+let color1 = document.querySelector("#color1");
+let color2 = document.querySelector("#color2");
+let direction = document.querySelector("#direction");
+let code = document.querySelector("#code");
+let form = document.querySelector("#form");
+let codeWrap = document.querySelector(".codeWrap");
+let copyCode = document.querySelector(".copy");
+let textInput;
 
-
-form.addEventListener('submit',(e)=>{
+form.addEventListener("submit", (e) => {
     e.preventDefault();
-
-
 
     let color1Val = color1.value;
     let color2Val = color2.value;
     let directionVal = direction.value;
 
-  
     mybackground = `linear-gradient(to ${directionVal}, ${color1Val}, ${color2Val})`;
-    
-    
-    codeWrap.style.display='block';
-    // code.textContent = mybackground;
 
+    codeWrap.style.display = "block";
+    code.textContent = `your bg code here : ${mybackground}`;
 
     document.body.style.backgroundImage = mybackground;
+    copyText(mybackground);
+});
 
-    
- 
-    copyText();
-
-
-
-})
-
-let textInput;
-function copyText() {
-
-
-
-    copyCode.addEventListener('click', (e) => {
-        e.preventDefault();
-        let text = mybackground;
-
-      
-         textInput = document.createElement('textarea');
-        
-
-        if (textInput.value.length===0) {
-            console.log('start first');
-            textInput.value = text;
+function copyText(mybackground) {
+    copyCode.addEventListener("click", () => {
+        if (document.querySelector("textarea") == null) {
+            textInput = document.createElement("textarea");
             codeWrap.append(textInput);
+            textInput.value = mybackground;
 
             textInput.select();
             document.execCommand("copy");
-           
 
-            alert('code copied successfully');
+            code.textContent = "code copied";
 
-
-        } else if (textInput.value.length > 0) {
-            console.log('start secnd');
-            console.log(textInput.value.length);
-            textInput.value = text;
+            setTimeout(() => {
+                code.hidden = true;
+            }, 1000);
+        } else {
+            textInput.value = mybackground;
 
             textInput.select();
             document.execCommand("copy");
-            alert('code copied successfully');
+            code.textContent = "code copied";
 
+            setTimeout(() => {
+                code.hidden = true;
+            }, 1000);
         }
-        else {
-            console.log('some thing wrong !');
-        }
-
-        
-    })
+    });
 }
